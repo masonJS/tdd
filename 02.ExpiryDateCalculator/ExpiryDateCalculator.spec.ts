718c4f11b6fde29dc285b083f4074955acadd787
minwoo <haruLife@inflearn.com>
2021-12-11 16:53:28 +0900

    만료일 계산기 - 첫번째 테스트

--- 02.ExpiryDateCalculator/ExpiryDateCalculator.spec.ts
+++ 02.ExpiryDateCalculator/ExpiryDateCalculator.spec.ts
@@ -1,3 +1,16 @@
-describe('만료일 계산기', () => {
+import { LocalDate } from "@js-joda/core";
+import { ExpiryDateCalculator } from "./ExpiryDateCalculator";
 
+describe('만료일 계산기', () => {
+  it.each([
+    [LocalDate.of(2021, 11, 1), LocalDate.of(2021, 12, 1)],
+    [LocalDate.of(2021, 12, 12), LocalDate.of(2022, 1, 12)],
+    [LocalDate.of(2019, 1, 31), LocalDate.of(2019, 2, 28)],
+    [LocalDate.of(2019, 1, 30), LocalDate.of(2019, 2, 28)],
+  ])('만원을 납부하면 한달뒤가 만료일이 된다.', (billingDate, expiryDate) => {
+    const payAmount = 10000;
+    const cal = new ExpiryDateCalculator();
+    const result = cal.calculateExpiryDate(billingDate, payAmount);
+    expect(result).toStrictEqual(expiryDate);
+  })
 })
--- 02.ExpiryDateCalculator/ExpiryDateCalculator.ts
+++ 02.ExpiryDateCalculator/ExpiryDateCalculator.ts
@@ -1,3 +1,7 @@
+import { LocalDate } from "@js-joda/core";
+
 export class ExpiryDateCalculator {
-  
+  calculateExpiryDate(billingDate: LocalDate, payAmount: number): LocalDate {
+    return billingDate.plusMonths(1);
+  }
 }
